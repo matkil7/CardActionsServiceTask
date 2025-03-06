7c1fa93a5a689633324872b9d8919b019899fff0
matkil7 <mateuszkiljan7@gmail.com>
2025-03-06 14:29:49 +0100

    added ActionService.cs and Result reference

---
+++ CardActionsApi/Services/ActionService.cs
@@ -0,0 +1,17 @@
+﻿using CardActionsApi.Models;
+using Utils;
+
+namespace CardActionsApi.Services;
+
+public interface IActionService
+{
+    Task<Result<IEnumerable<string>>> GetCardActions(string userId, string cardNumber);
+}
+
+public class ActionService : IActionService
+{
+    public Task<Result<IEnumerable<string>>> GetCardActions(string userId, string cardNumber)
+    {
+        throw new NotImplementedException();
+    }
+}
