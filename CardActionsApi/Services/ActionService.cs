9ba29fac5eda8531390934259d30d8f8dae52627
matkil7 <mateuszkiljan7@gmail.com>
2025-03-06 14:56:05 +0100

    added ActionService.cs

--- CardActionsApi/Services/ActionService.cs
+++ CardActionsApi/Services/ActionService.cs
@@ -1,4 +1,6 @@
 ﻿using CardActionsApi.Models;
+using CardActionsApi.Providers;
+using CardActionsApi.Specifications;
 using Utils;
 
 namespace CardActionsApi.Services;
@@ -10,6 +12,22 @@ public interface IActionService
 
 public class ActionService : IActionService
 {
+    private readonly Dictionary<int, ISpecificationBuilder<CardDetails>> _specifications;
+    private readonly ICardService _cardService;
+
+    public ActionService(ISpecificationsProvider<CardDetails> actionDefinitionProvider, ICardService cardService)
+    {
+        _specifications = actionDefinitionProvider.GetDefinitions();
+        _cardService = cardService; 
+    }
+    
+    public IEnumerable<string> GetActions(CardDetails cardDetails)
+    {
+        return _specifications
+            .Where(rule => rule.Value.IsValid(cardDetails))
+            .Select(rule => $"ACTION{rule.Key}")
+            .ToList();
+    }
     public Task<Result<IEnumerable<string>>> GetCardActions(string userId, string cardNumber)
     {
         throw new NotImplementedException();
