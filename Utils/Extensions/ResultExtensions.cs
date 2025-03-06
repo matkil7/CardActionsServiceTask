aefcbc5b4ad649fe426ebc0350620a9e2d1a5a31
matkil7 <mateuszkiljan7@gmail.com>
2025-03-06 16:35:21 +0100

    result utils extended

---
+++ Utils/Extensions/ResultExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Utils.Extensions;
+
+public static class ResultExtensions
+{
+    public static TTarget Match<TSource, TTarget>(
+        this Result<TSource> result,
+        Func<TSource, TTarget> onSuccess,
+        Func<string[], TTarget> onFailure)
+    {
+        return result.IsSuccess ? onSuccess(result.Value) : onFailure(result.Errors);
+    }
+}
