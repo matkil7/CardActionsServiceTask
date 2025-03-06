a8e6bfe4fc930afd01060d3437d38a325fd1416f
matkil7 <mateuszkiljan7@gmail.com>
2025-03-06 16:23:51 +0100

    renamed AnyOf to IsSatisfiableEnum class

---
+++ CardActionsApi/Helpers/ActionSpecifiactionHelper.cs
@@ -0,0 +1,16 @@
+﻿using CardActionsApi.Models;
+using CardActionsApi.Specifications;
+using CardActionsApi.Specifications.Actions;
+using CardActionsApi.Specifications.Rules.Enums;
+
+namespace CardActionsApi.Helpers;
+
+public class ActionSpecifiactionHelper
+{
+    public static ISpecificationBuilder<CardDetails> AccessibleCardTypeAndAnyOfState(CardStatus[] statuses) =>
+        new ActionSpecificationBuilder().Rule(x=> Enum.GetValues<CardType>().Contains(x.CardType)).Rule(new AnyOfStatuses(statuses));
+    public static ISpecificationBuilder<CardDetails> AccessibleCardTypeAndAccessibleState() =>
+        new ActionSpecificationBuilder().Rule(x => Enum.GetValues<CardType>().Contains(x.CardType)).Rule(x => Enum.GetValues<CardStatus>().Contains(x.CardStatus));
+    public static ISpecificationBuilder<CardDetails> AnyOfCardTypeAndAccessibleState(CardType[] types) =>
+        new ActionSpecificationBuilder().Rule(new AnyOfTypes(types)).Rule(x => Enum.GetValues<CardStatus>().Contains(x.CardStatus));
+}
--- CardActionsApi/Specifications/Rules/IsSatisfiableEnum.cs
+++ CardActionsApi/Specifications/Rules/IsSatisfiableEnum.cs
@@ -1,9 +1,9 @@
 ﻿namespace CardActionsApi.Specifications.Rules;
 
-public abstract class AnyOf<T, TModel> : ISpecification<TModel> where T :  Enum{
+public abstract class IsSatisfiableEnum<T, TModel> : ISpecification<TModel> where T :  Enum{
     protected T[] _items { get; init; }
     
-    public AnyOf (T[] items)
+    public IsSatisfiableEnum (T[] items)
     {
         _items = items;
     }
