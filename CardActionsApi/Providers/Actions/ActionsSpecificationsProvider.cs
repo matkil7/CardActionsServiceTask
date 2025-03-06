7f2eaff4041b74f0900e097fc8bfe7d95ef20f1c
matkil7 <mateuszkiljan7@gmail.com>
2025-03-06 16:24:30 +0100

    added action specifications

--- CardActionsApi/Providers/Actions/ActionsSpecificationsProvider.cs
+++ CardActionsApi/Providers/Actions/ActionsSpecificationsProvider.cs
@@ -1,12 +1,66 @@
-﻿using CardActionsApi.Models;
+﻿using CardActionsApi.Helpers;
+using CardActionsApi.Models;
 using CardActionsApi.Specifications;
+using CardActionsApi.Specifications.Actions;
+using CardActionsApi.Specifications.Rules.Enums;
 
 namespace CardActionsApi.Providers.Actions;
 
-public class ActionsSpecificationsProvider : ISpecificationsProvider<CardDetails>   
+public class ActionsSpecificationsProvider : ISpecificationsProvider<CardDetails>
 {
     public Dictionary<int, ISpecificationBuilder<CardDetails>> GetDefinitions()
     {
-        throw new NotImplementedException();
+        return new Dictionary<int, ISpecificationBuilder<CardDetails>>
+        {
+            {
+                1, ActionSpecifiactionHelper.AccessibleCardTypeAndAnyOfState([CardStatus.Active])
+            },
+            {
+                2, ActionSpecifiactionHelper.AccessibleCardTypeAndAnyOfState([CardStatus.Inactive])
+            },
+            {
+                3, ActionSpecifiactionHelper.AccessibleCardTypeAndAccessibleState()
+            },
+            { 4, ActionSpecifiactionHelper.AccessibleCardTypeAndAccessibleState() },
+            {
+                5,
+                ActionSpecifiactionHelper.AnyOfCardTypeAndAccessibleState([CardType.Credit])
+            },
+            {
+                6,
+                new ActionSpecificationBuilder().Rule(x => x.CardType == CardType.Credit).Rule(x =>
+                    x.IsPinSet && CardStatusHelper.OrderedInactiveActiveBlocked.Contains(x.CardStatus))
+            },
+            {
+                7, new ActionSpecificationBuilder().Rule(x => CarTypeHelper.CardTypes.Contains(x.CardType)).Rule(x =>
+                    !x.IsPinSet && CardStatusHelper.OrderedInactiveActiveBlocked.Contains(x.CardStatus))
+            },
+            {
+                8,
+                new ActionSpecificationBuilder().Rule(new IsAccessibleStatus())
+                    .Rule(new AnyOfStatuses(CardStatusHelper.OrderedInactiveActiveBlocked))
+            },
+            { 9, new ActionSpecificationBuilder().Rule(new IsAccessibleType()).Rule(new IsAccessibleStatus()) },
+            {
+                10,
+                new ActionSpecificationBuilder().Rule(new IsAccessibleType())
+                    .Rule(new AnyOfStatuses(CardStatusHelper.OrderedInactiveActive))
+            },
+            {
+                11, new ActionSpecificationBuilder().Rule(new IsAccessibleType()).Rule(new AnyOfStatuses(
+                    CardStatusHelper.InactiveActive
+                ))
+            },
+            {
+                12, new ActionSpecificationBuilder().Rule(new IsAccessibleType()).Rule(new AnyOfStatuses(
+                    CardStatusHelper.OrderedInactiveActive
+                ))
+            },
+            {
+                13,
+                new ActionSpecificationBuilder().Rule(new IsAccessibleType())
+                    .Rule(new AnyOfStatuses(CardStatusHelper.OrderedInactiveActive))
+            },
+        };
     }
 }
