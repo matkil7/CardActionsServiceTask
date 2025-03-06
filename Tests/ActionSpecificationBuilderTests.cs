bd7c1583ca64c0f669a568e38e37dd471e5e9133
matkil7 <mateuszkiljan7@gmail.com>
2025-03-06 14:43:41 +0100

    added ActionSpecificationBuilder UT scaffold

---
+++ Tests/ActionSpecificationBuilderTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Tests;
+
+public class ActionSpecificationBuilderTests
+{
+    [Fact]
+    public void IsValid_NoRulesDefined_ReturnsTrue()
+    {
+    }
+
+    [Fact]
+    public void IsValid_AllSpecificationsAndConditionsPass_ReturnsTrue()
+    {
+    }
+
+    [Fact]
+    public void IsValid_AnySpecificationFails_ReturnsFalse()
+    {
+    }
+
+    [Fact]
+    public void IsValid_AnyConditionFails_ReturnsFalse()
+    {
+    }
+
+    [Fact]
+    public void IsValid_MultipleSpecificationsAndConditionsPass_ReturnsTrue()
+    {
+    }
+}
