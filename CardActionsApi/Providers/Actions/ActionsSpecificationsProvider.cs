e12d1c8a47c7fcfdc260d94d71907cd2312a955a
matkil7 <mateuszkiljan7@gmail.com>
2025-03-06 14:53:27 +0100

    added ISpecificationsProvider.cs and ActionsSpecificationsProvider.cs

---
+++ CardActionsApi/Providers/Actions/ActionsSpecificationsProvider.cs
@@ -0,0 +1,12 @@
+﻿using CardActionsApi.Models;
+using CardActionsApi.Specifications;
+
+namespace CardActionsApi.Providers.Actions;
+
+public class ActionsSpecificationsProvider : ISpecificationsProvider<CardDetails>   
+{
+    public Dictionary<int, ISpecificationBuilder<CardDetails>> GetDefinitions()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ CardActionsApi/Providers/ISpecificationsProvider.cs
@@ -0,0 +1,8 @@
+﻿using CardActionsApi.Specifications;
+
+namespace CardActionsApi.Providers;
+
+public interface ISpecificationsProvider<T> where T : class
+{
+    Dictionary<int, ISpecificationBuilder<T>> GetDefinitions();
+}
