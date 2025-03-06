b241ee0d3526d6a6cd1165f24d972b7177994979
matkil7 <mateuszkiljan7@gmail.com>
2025-03-06 14:41:51 +0100

    added ActionSpecificationBuilder.cs

---
+++ CardActionsApi/Specifications/Actions/ActionSpecificationBuilder.cs
@@ -0,0 +1,24 @@
+﻿using CardActionsApi.Models;
+
+namespace CardActionsApi.Specifications.Actions;
+
+public class ActionSpecificationBuilder : ISpecificationBuilder<CardDetails>
+{
+    private readonly List<ISpecification<CardDetails>> _specifications = new();
+    private readonly List<Func<CardDetails, bool>> _conditions = new();    
+    
+    public ISpecificationBuilder<CardDetails> Rule(ISpecification<CardDetails> spec)
+    {
+        _specifications.Add(spec);
+        return this;
+    } 
+    public ISpecificationBuilder<CardDetails> Rule(Func<CardDetails, bool> condition)
+    {
+        _conditions.Add(condition);
+        return this;
+    }
+    public bool IsValid(CardDetails details)
+    {
+        return _specifications.All(spec => spec.IsSatisfiedBy(details) && _conditions.All(condition => condition(details)));
+    }
+}
